0212c27d63e299d2972517b48355deebe75dfcb3
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-11-29 02:13:08 +0100

    fixed some bug in main methode

--- src/main/java/dev/guadalupe/katavehiclesandtolls/KatavehiclesandtollsApplication.java
+++ src/main/java/dev/guadalupe/katavehiclesandtolls/KatavehiclesandtollsApplication.java
@@ -4,6 +4,8 @@ package dev.guadalupe.katavehiclesandtolls;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 import dev.guadalupe.katavehiclesandtolls.car.Car;
 import dev.guadalupe.katavehiclesandtolls.motorbike.Motorbike;
 import dev.guadalupe.katavehiclesandtolls.truck.Truck;
@@ -13,21 +15,23 @@ import dev.guadalupe.katavehiclesandtolls.vehicle.Vehicle;
 public class KatavehiclesandtollsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KatavehiclesandtollsApplication.class, args);
-	}
-        // Crear instancias de los vehículos
-        Vehicle motorbike = new Motorbike(1, "123ABC");
-        Vehicle car = new Car(2, "456DEF");
-        Vehicle truck = new Truck(3, "789GHI", 4);
+    SpringApplication.run(KatavehiclesandtollsApplication.class, args);
 
-        // Calcular Tolls
-        motorbike.calculateToll();
-        car.calculateToll();
-        truck.calculateToll();
+    // Crear y manejar vehículos
+    Vehicle motorbike = new Motorbike(1, "123ABC");
+    Vehicle car = new Car(2, "456DEF");
+    Vehicle truck = new Truck(3, "789GHI", 4);
 
-        // Mostrar resultados
-        System.out.println("motorbike - Toll: $" + motorbike.getToll());
-        System.out.println("car - Toll: $" + car.getToll());
-        System.out.println("Camión - Toll: $" + truck.getToll());
+    // Calcular el importe para cada vehículo
+    motorbike.calculateToll();
+    car.calculateToll();
+    truck.calculateToll();
+
+    // Mostrar los resultados
+    System.out.println("Motorbike - Toll: $" + motorbike.getToll());
+    System.out.println("Car - Toll: $" + car.getToll());
+    System.out.println("Truck - Toll: $" + truck.getToll());
+}
+
    }
 
