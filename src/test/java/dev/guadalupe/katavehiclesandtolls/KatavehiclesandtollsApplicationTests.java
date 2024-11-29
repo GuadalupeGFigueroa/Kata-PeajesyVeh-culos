1bef9c491081b1ac6dfb1cdc7cb99aef62af4053
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-11-29 03:20:16 +0100

    Dependency junit.contrib added inside the POM.

--- src/test/java/dev/guadalupe/katavehiclesandtolls/KatavehiclesandtollsApplicationTests.java
+++ src/test/java/dev/guadalupe/katavehiclesandtolls/KatavehiclesandtollsApplicationTests.java
@@ -1,13 +1,30 @@
 package dev.guadalupe.katavehiclesandtolls;
 
 import org.junit.jupiter.api.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class KatavehiclesandtollsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+public class KatavehiclesandtollsApplicationTests {
+     
+    @Test
+    public void testRunApplication() {
+        // Captura la salida del método runApplication()
+        SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+        
+        // Ejecuta el método
+        KatavehiclesandtollsApplication.runApplication();
 
-}
+        // Verifica que los mensajes esperados se imprimen
+        String output = systemOutRule.getLog();
+        assertThat(output, containsString("Motorbike - Toll: $50"));
+        assertThat(output, containsString("Car - Toll: $100"));
+        assertThat(output, containsString("Truck - Toll: $200"));
+    }
+    }
+    
+
