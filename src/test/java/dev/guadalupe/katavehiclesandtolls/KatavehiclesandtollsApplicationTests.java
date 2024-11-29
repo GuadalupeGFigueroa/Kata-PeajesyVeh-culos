8d7c43577812e5169c8955243286b7e18908cdcc
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-11-29 19:05:45 +0100

    Fixed errors in KatavehiclesandtollsApplication test

--- src/test/java/dev/guadalupe/katavehiclesandtolls/KatavehiclesandtollsApplicationTests.java
+++ src/test/java/dev/guadalupe/katavehiclesandtolls/KatavehiclesandtollsApplicationTests.java
@@ -1,10 +1,11 @@
 package dev.guadalupe.katavehiclesandtolls;
 
 import org.junit.jupiter.api.Test;
-import org.junit.contrib.java.lang.system.SystemOutRule;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+
+import org.junit.jupiter.api.Test;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,19 +13,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class KatavehiclesandtollsApplicationTests {
      
     @Test
-    public void testRunApplication() {
-        // Captura la salida del método runApplication()
-        SystemOutRule systemOutRule = new SystemOutRule().enableLog();
-        
-        // Ejecuta el método
-        KatavehiclesandtollsApplication.runApplication();
+    public void testRunApplication() throws Exception {
+        // Captura la salida del método runApplication() usando SystemLambda
+        String output = tapSystemOut(() -> {
+            KatavehiclesandtollsApplication.runApplication();
+        });
 
         // Verifica que los mensajes esperados se imprimen
-        String output = systemOutRule.getLog();
         assertThat(output, containsString("Motorbike - Toll: $50"));
         assertThat(output, containsString("Car - Toll: $100"));
         assertThat(output, containsString("Truck - Toll: $200"));
     }
-    }
+}
+    
     
 
